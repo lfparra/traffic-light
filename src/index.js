dd225e2f28b65dd9169e412e4acbdc0427b2c10d
Leonardo Parra <leoparra95@gmail.com>
2020-06-03 18:32:53 -0400

    Proyecto finalizado

--- src/index.js
+++ src/index.js
@@ -1,25 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
 const TrafficLight = props => {
+
+  const [luzActiva, setLuzActiva] = useState("");
+  
+  const cambiarLuzActiva = (e) => {
+     console.log(e.target.className);
+     setLuzActiva(e.target.className);
+  }
+
+  let resultadoRed = luzActiva === "red-light" ? " red-light-active" : "";
+  let resultadoYellow = luzActiva === "yellow-light" ? " yellow-light-active" : "";
+  let resultadoGreen = luzActiva === "green-light" ? " green-light-active" : "";
+  
   return (
     <div className="semaforo">
-        <div className="red-light"></div>
-        <div className="yellow-light"></div>
-        <div className="green-light"></div>
+        <div className={"red-light" + resultadoRed} onClick={(e) => cambiarLuzActiva(e)}></div>
+        <div className={"yellow-light" + resultadoYellow} onClick={(e) => cambiarLuzActiva(e)}></div>
+        <div className={"green-light" + resultadoGreen} onClick={(e) => cambiarLuzActiva(e)}></div>
     </div>
   )
 }
-
-
 
 const App = () => {
   return (
     <>
     <h2>Traffic Light</h2>
-    <TrafficLight />
-    <TrafficLight />
+    <div className="container">
+      <TrafficLight />
+      <TrafficLight />
+      <TrafficLight />
+    </div>
     </>
   )
 }
